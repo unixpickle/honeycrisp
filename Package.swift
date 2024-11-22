10ea7609dad7578309603af0846117e2749db99d
Alex Nichol <unixpickle@gmail.com>
2024-11-22 13:48:04 -0500

    delete deps

--- Package.swift
+++ Package.swift
@@ -16,16 +16,11 @@ let package = Package(
     .library(
       name: "Honeycrisp",
       targets: ["Honeycrisp"]),
-    .library(
-      name: "MNIST",
-      targets: ["MNIST"]),
   ],
   dependencies: [
     .package(
       url: "https://github.com/swiftlang/swift-syntax.git", "509.0.0"..<"601.0.0-prerelease"),
     .package(url: "https://github.com/1024jp/GzipSwift", "6.0.0"..<"6.1.0"),
-    .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
-    .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0"..<"4.0.0"),
     .package(url: "https://github.com/unixpickle/coreml-builder.git", "0.1.0"..<"0.2.0"),
     .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
   ],
@@ -58,22 +53,6 @@ let package = Package(
       ]),
     .executableTarget(
       name: "HCMatrixBench",
-      dependencies: ["Honeycrisp"]),
-    .target(
-      name: "MNIST",
-      dependencies: [
-        .product(name: "AsyncHTTPClient", package: "async-http-client"),
-        .product(name: "Gzip", package: "GzipSwift"),
-        .product(name: "Crypto", package: "swift-crypto"),
-      ]),
-    .executableTarget(
-      name: "MNISTExample",
-      dependencies: ["MNIST", "Honeycrisp"]),
-    .executableTarget(
-      name: "MNISTGenExample",
-      dependencies: ["MNIST", "Honeycrisp"]),
-    .executableTarget(
-      name: "Text2Image",
       dependencies: ["Honeycrisp"]),
   ]
 )
